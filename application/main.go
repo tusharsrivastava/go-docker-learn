bf70f7848bba0bf5c19c0e97d37381a1a530394f
Tushar Srivastava <razieldecarte@gmail.com>
2021-05-17 03:21:29 +0530

    Changed the go code to a simple HTTP server

--- application/main.go
+++ application/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	PORT := ":" + os.Getenv("PORT")
+	http.HandleFunc("/", handleIndex)
+	log.Println("Server Listening on :", PORT)
+	if err := http.ListenAndServe(PORT, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from Go!")
 }
