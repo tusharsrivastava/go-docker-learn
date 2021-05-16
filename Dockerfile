f8e290050936480959f0435a53fda495cd15865a
Tushar Srivastava <razieldecarte@gmail.com>
2021-05-17 04:12:26 +0530

    Added a test audio file. Now building the streaming service over it

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,8 @@ FROM alpine:3.13 AS deploy
 
 COPY --from=build /application/dist /application
 
+COPY --from=build /application/data /application/data
+
+RUN apk add  --no-cache ffmpeg
+
 CMD ["/application/go-docker-learn"]
--- application/main.go
+++ application/main.go
@@ -17,5 +17,35 @@ func main() {
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from Go!")
+	fp, err := os.OpenFile("/application/data/testfile.mp3", os.O_RDONLY, 0755)
+
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	defer fp.Close()
+
+	stats, err := fp.Stat()
+
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	var b []byte = make([]byte, stats.Size())
+
+	bytes, err := fp.Read(b)
+
+	log.Printf("%d bytes read", bytes)
+
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	w.Write(b)
 }
